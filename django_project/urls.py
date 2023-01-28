cd4bf4b153053bfd40f1a3cdef62c1d0f3dafba5
Xavi Flores <xa_franc@live.com>
2023-01-28 12:26:08 -0600

    chapter10

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", include("pages.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
